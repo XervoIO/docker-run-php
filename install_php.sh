89e33a08e68f298790ec7eaac4a3a5932ed9ee71
Ken Herner <chosenken@gmail.com>
2015-10-15 18:21:54 -0400

    Update FROM for new naming convention
    Fix issue where bootstrap.sh would not complete
    
    Update for new FROM image name convention
    
    Revert "Fix issue where bootstrap.sh would not complete"
    
    This reverts commit a88cea7ed5f3c2078129ea5e60b4170e770dd618.
    
    Fix issue where bootstrap.sh would not complete
    
    Fix bootstrap.sh to build and install php
    
    Bootstrap.sh will now build and install php 5.3, 5.4, 5.5, and 5.6, and
    place them in /opt/modulus/php.  It also sets "clear_env" to "no" in the
    php-fpm.conf file for php 5.4, 5.5 and 5.6.  The variable is not present
    in 5.3.
    
    Update bootstrap
    
    Added set -e and -x, fixed an issue cd'ing to home.  Also using latest
    php version.
    
    Add support for mongo, gmp, and soap to php.
    
    Add check for tailing build log when build fails
    
    Update init script to report on mongo install
    
    Revert php versions to current supported versions
    
    Update home dir to /root
    
    Single version image, PHP 5.6
    
    Testing running the scritp as two user
    
    Install PHP as mop user instead of root.
    
    Fix up script to install php correctly
    
    Move home dir out of /mnt/home
    
    /mnt/home gets mounted as a volume when run in modulus, so we lose the
    php install.
    
    Update script to php 5.6.14
    
    Updated start script to properly update php config files when a user
    specifies their own files.
    
    Add comments to start script

---
+++ install_php.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+set -x
+
+# Variables
+HOME=/home/mop
+PHP_BREW_DIR=$HOME/.phpbrew
+PHP_INSTALL_DIR=/opt/modulus/php
+TEMP_DIR=$HOME/tmp
+TMP_DIR=$TEMP_DIR
+TMPDIR=$TEMP_DIR
+PHP_BREW_FLAGS="+default +mysql +pgsql +fpm +soap +gmp -- \
+  --with-libdir=lib/x86_64-linux-gnu --with-gd=shared --enable-gd-natf \
+  --with-jpeg-dir=/usr --with-png-dir=/usr"
+
+# Allows compiling with all cpus
+export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
+
+# phpbrew must be initialized to work.  Will create the folder
+# ~/.phpbrew
+phpbrew init
+source ~/.phpbrew/bashrc
+
+# Install PHP
+if ! phpbrew install php-$PHP_VER $PHP_BREW_FLAGS ; then
+  if [ -f $PHP_BREW_DIR/build/php-$PHP_VER/build.log ]; then
+    tail -200 $PHP_BREW_DIR/build/php-$PHP_VER/build.log
+    exit 1
+  else
+    echo "Build failed, no log file created."
+    exit 1
+  fi
+fi
+
+# Install MONGO support
+# NOTE:  We run this out of the other loop because if we run this in the other
+# loop, the installation of PHP 5.4 will almost always fail because reasons.
+# (I really don't know why it fails, phpbrew just fails to install it)
+phpbrew use $PHP_VER
+if ! phpbrew ext install mongo ; then
+  echo "Installing mongo failed for $PHP_VER"
+  if [ -f $PHP_BREW_DIR/build/php-$PHP_VER/ext/mongo/build.log ]; then
+    tail -200 $PHP_BREW_DIR/build/php-$PHP_VER/ext/mongo/build.log
+    exit 1
+  else
+    echo "Mongo build log missing"
+    exit 1
+  fi
+fi
