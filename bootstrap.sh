b38007edb2e597edeb0e426e9f8093fcb7699c28
Ken Herner <chosenken@gmail.com>
2015-10-21 15:26:31 -0400

    Fix bootstrap.sh to build and install php
    Bootstrap.sh will now build and install php 5.3, 5.4, 5.5, and 5.6, and
    place them in /opt/modulus/php.  It also sets "clear_env" to "no" in the
    php-fpm.conf file for php 5.4, 5.5 and 5.6.  The variable is not present
    in 5.3.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-set -e
-set -x
 
-export HOME=/root
-export TEMP_DIR=/root/tmp
+# PHP Version to build.  We build 5.3, 5.4, 5.5, and 5.6.
+PHP_5_3="5.3.29"
+PHP_5_4="5.4.45"
+PHP_5_5="5.5.30"
+PHP_5_6="5.6.14"
+
+PHP_BREW_DIR=/mnt/home/.phpbrew
+export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
+PHP_INSTALL_DIR=/opt/modulus/php
+PHP_BREW_FLAGS="+default +mysql +pgsql +fpm -- \
+  --with-libdir=lib/x86_64-linux-gnu --with-gd=shared --enable-gd-natf \
+  --with-jpeg-dir=/usr --with-png-dir=/usr"
 
 # Install nginx
 echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
@@ -18,9 +26,9 @@ mkdir /var/lib/nginx/proxy
 mkdir /var/lib/nginx/fastcgi
 mkdir /var/lib/nginx/uwsgi
 mkdir /var/lib/nginx/scgi
+mkdir -p $PHP_INSTALL_DIR
 
-# Install php-brew
-export MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
+# Install php 5 to run phpbrew
 mkdir /mnt/tmp
 apt-get build-dep -y php5
 apt-get install -y php5 php5-dev php-pear autoconf automake php5-gd curl \
@@ -35,6 +43,21 @@ apt-get install -y php5 php5-dev php-pear autoconf automake php5-gd curl \
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod +x phpbrew
 mv phpbrew /usr/bin/phpbrew
+cd $HOME
 
-# tar -C /opt/modulus -xvzf /opt/modulus/php-4212015.tar.gz
-# rm -rf /opt/modulus/php-4212015.tar.gz
+phpbrew init
+
+PHP_VERS=($PHP_5_3 $PHP_5_4 $PHP_5_5 $PHP_5_6)
+# Install PHP
+for PHP_VER in "${PHP_VERS[@]}"
+do
+  if phpbrew install php-$PHP_VER $PHP_BREW_FLAGS ; then
+    mv $PHP_BREW_DIR/php/php-$PHP_VER $PHP_INSTALL_DIR/php-$PHP_VER
+    echo "clear_env = no" >> $PHP_INSTALL_DIR/php-$PHP_VER/etc/php-fpm.conf
+  else
+    tail -200 $PHP_BREW_DIR/build/php-$PHP_VER/build.log
+  fi
+done
+
+# Clean up
+rm -rf $PHP_BREW_DIR
